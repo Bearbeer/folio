9de892902735006ff566b0074b7b5051879569c1
Nagion <skrgus2004@naver.com>
2020-04-02 15:45:35 +0900

    Skill name 길이 조건 추가

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -7,7 +7,12 @@ class Skill < ActiveRecord::Base
   belongs_to :user
 
   # portfolio_id, user_id는 Association을 통해 검증
-  validates :name, presence: { message: '이름이 존재하지 않음' }
+  validates :name, presence: { message: '이름이 존재하지 않음' },
+            length: { 
+              maximum: 100,
+              message: '이름이 100글자를 초과'
+            }
+            
   validates :level, presence: { message: '레벨이 존재하지 않음' },
             numericality: { 
               only_integer: true,
