97bb82dfcdabf061143b20ad219ce8959524b98a
Eun <Eun>
2020-04-02 15:22:33 +0900

    :fn 붙이기

--- app/models/portfolio_carrer.rb
+++ app/models/portfolio_carrer.rb
@@ -1,23 +1,22 @@
-class PortfolioCareer < ActiveRecord::base
-    acts_as_paranoid
+class PortfolioCareer < ActiveRecord::Base
+  acts_as_paranoid
   
-    self.table_name = :portfolio_careers
+  self.table_name = :portfolio_careers
   
-    belongs_to :portfolio
-    belongs_to :user
+  belongs_to :portfolio
+  belongs_to :user
   
   # name, description, start_date, end_date
   
-    validates :name, presence: {message : '경력명이 존재하지 않음'}
-    validate : end_date_after_start_date
+  validates :name, presence: {message : '경력명이 존재하지 않음'}
+  validate :end_date_after_start_date
   
-    private
+  private
   
-    def end_date_after_start_date
-      return if start_date.blank? ||  end_date.blank?
-      
-      if end_date < start_date
-        raise '날짜 설정 오류'
+  def end_date_after_start_date
+    return if start_date.blank? ||  end_date.blank?
     
-  end
-  
+    if end_date < start_date
+      raise '날짜 설정 오류'
+  end 
+end
