99f482f9372f3b39f9acee85890a426c90808cd5
ksee1230 <ksee1230@gmail.com>
2020-04-02 16:08:27 +0900

    education 모델 1차 수정

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,12 +1,10 @@
-require 'bcrypt'
-
-class education < ActiveRecord::Base
+class Education < ActiveRecord::Base
   # deleted_at 기반 소프트 딜리트 기능 이용
   acts_as_paranoid
 
   self.table_name = :portfolio_educations
 
-  belongs_to :users
+  belongs_to :user
   belongs_to :portfolio
 
   STATUS = {
@@ -20,20 +18,20 @@ class education < ActiveRecord::Base
   validates :name, presence: { message: '학교명이 존재하지 않음' }
   validates :start_date, presence: { message: '시작일자가 존재하지 않음' }
 
-  after_validation :validate_end_date, :validate_date
+  validate :validate_end_date, :validate_date
 
   private
 
   # status와 end_date 관계 확인
   def validate_end_date
-    return unless status == graduated ^ end_date.present?
+    return unless (status == graduated) ^ end_date.present?
 
     raise '종료일자 정책 위반'
   end
 
   # start_date < end_date 확인
   def validate_date
-    return if start_date < end_date || !end_date.present?
+    return if (start_date < end_date) || !end_date.present?
 
     raise '시작일자, 종료일자 재설정'
   end
