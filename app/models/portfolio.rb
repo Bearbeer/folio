19e0dd36436819284093012de8b0ecd08f8aa0b0
Eun <Eun>
2020-04-06 01:25:59 +0900

    enum

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,20 +1,30 @@
-class Portfolio < ActiveRecord::Basea
-    act_as_paranoid
+class Portfolio < ActiveRecord::Base
 
-    self.table_name = :portfolios
+  acts_as_paranoid
 
-    belongs_to :user
-		
-		#gender enum 
-		enum GENDER:  {male: '남자', female: '여자'}
-		validates :gender, :inclusion => {in: GENDER.keys}
-    #공유 가능상태
-    def shared_link
-			return false if public_code == ''
-			return self.public_code
+  self.table_name = :portfolios
 
+  belongs_to :user
 
-		end
+  # gender enum
+  validates :title, length: {maximum: 500}
+  validates :name, length: {maximum: 100}
+  validates :mobile, length: {maximum: 100}
+  validates :email, length: {maximum: 500}
 
+  @male = 0
+  @female = 1
+  enum gen: { male: 0, female: 1 }
+  validates :gender, :inclusion => { in: %w[남자 여자] }
+  validates :address, length: { maximum: 1000 }
+  validates :memo, length: { maximum: 5000 }
 
+  # shareable
+  
+  def shared_link
+
+    return false if :public_code == ''
+
+    :public_code
+  end
 end
