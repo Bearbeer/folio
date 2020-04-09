8670e9c1642f927fb0e74be6673432e21b60211e
Bearbeer <kyu2926@gmail.com>
2020-04-09 15:28:29 +0900

    Session/Login 컨트롤러 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 로그인 / 로그아웃
+  resources :sessions, controller: 'session/main', only: [] do
+    collection do
+      post :login
+      post :logout
+    end
+
+  end
 end
