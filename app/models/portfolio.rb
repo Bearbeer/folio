35774bee78472731f5ed0ac3a526f95fba3f7ed0
Eun <Eun>
2020-04-09 14:50:10 +0900

    enum

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,20 +7,17 @@ class Portfolio < ActiveRecord::Base
   belongs_to :user
 
   # gender enum
-  validates :title, length: {maximum: 500}
-  validates :name, length: {maximum: 100}
-  validates :mobile, length: {maximum: 100}
-  validates :email, length: {maximum: 500}
+  validates :title, length: { maximum: 500 }
+  validates :name, length: { maximum: 100 }
+  validates :mobile, length: { maximum: 100 }
+  validates :email, length: { maximum: 500 }
 
-  @male = 0
-  @female = 1
-  enum gen: { male: 0, female: 1 }
-  validates :gender, :inclusion => { in: %w[남자 여자] }
+  GEN = %w[남자 여자]
+  validates :gender, inclusion: { in: GEN }
   validates :address, length: { maximum: 1000 }
   validates :memo, length: { maximum: 5000 }
 
   # shareable
-  
   def shared_link
 
     return false if :public_code == ''
