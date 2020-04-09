aa6c49ae4f8e8b25998b9adb9ef710207a3a18c2
Nagion <skrgus2004@naver.com>
2020-04-09 19:06:26 +0900

    로그인, 로그아웃 구현

--- app/controllers/session/main_controller.rb
+++ app/controllers/session/main_controller.rb
@@ -5,22 +5,22 @@ module Session
     before_action :validate_authorization, only: [:logout]
 
     def login
+      raise Exceptions::BadRequest if request.headers['Authorization'].present?
+
+      user_params = params.permit(:username, :password)
+      raise Exceptions::BadRequest unless user_params.permitted? and user_params.has_key?(:username) and user_params.has_key?(:password)
+
       user = User.find_by(username: user_params[:username].downcase)
-      raise Exceptions::NotFound unless user
-      raise Exceptions::Unauthorized unless user.authenticate(user_params[:password])
+      raise Exceptions::NotFound unless user and user.authenticate(user_params[:password])
 
       json(code: 200, data: { token: session_token(user) })
     end
 
     def logout
-
+      json(code: 204)
     end
 
     private
-
-    def user_params
-      params.require(:user).permit(:username, :password)
-    end
 
     def session_token(user)
       JWT.encode({ user_id: user.id, exp: 30.days.from_now.to_i }, ENV['SECRET_KEY_BASE'])
