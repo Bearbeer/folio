5dc26fa9a6b458dc85981ffee67ae07090dada87
Bearbeer <kyu2926@gmail.com>
2020-04-20 00:03:11 +0900

    UserController 추가: 회원가입, 회원탈퇴 API 추가

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# 회원가입/탈퇴 기능을 관리하는 컨트롤러
+class UserController < ApiController
+  include UserHelper
+
+  before_action :validate_authorization, only: :destroy
+
+  # POST /users
+  def create
+    validate_create_params
+    validate_username
+    user = User.create!(username: params[:username], password: params[:password])
+
+    json(code: 200, data: user_view(user))
+  end
+
+  # DELETE /users/:id
+  def destroy
+    validate_user_id
+    current_user.destroy
+
+    json(code: 200, message: '회원탈퇴가 완료되었습니다.')
+  end
+
+  private
+
+  def validate_create_params
+    params.require(:username)
+    params.require(:password)
+  end
+
+  def validate_username
+    return unless User.exists?(username: params[:username].downcase)
+
+    raise Exceptions::Conflict, '이미 존재하는 아이디입니다.'
+  end
+
+  def validate_user_id
+    params.require(:id)
+    return if current_user.id.to_i == params[:id].to_i
+
+    raise Exceptions::Forbidden, '권한이 없습니다.'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 로그인
-  resources :sessions, controller: 'session', only: [:create]
+  resources :sessions, controller: 'session', only: %i[create]
 
+  # 회원가입 / 회원탈퇴
+  resources :users, controller: 'user', only: %i[create destroy]
 end
