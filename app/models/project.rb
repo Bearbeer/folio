f95ae7f8561cb6ec10ef38ea2b21a521900e3040
Bearbeer <kyu2926@gmail.com>
2020-04-22 22:51:09 +0900

    중앙 프로젝트 모델 추가

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,34 +1,28 @@
 # frozen_string_literal: true
 
-class Portfolio
-  # 프로젝트 모델
-  class Project < ActiveRecord::Base
-    acts_as_paranoid
+# 프로젝트 모델
+class Project < ActiveRecord::Base
+  acts_as_paranoid
 
-    self.table_name = :portfolio_projects
+  self.table_name = :projects
 
-    MAX_NAME_SIZE = 100
-    MAX_DESCRIPTION_SIZE = 2000
+  MAX_NAME_SIZE = 100
+  MAX_DESCRIPTION_SIZE = 2000
 
-    belongs_to :user, class_name: 'User'
+  belongs_to :user, class_name: 'User'
+  belongs_to :portfolio, class_name: 'Portfolio'
 
-    validates :user, presence: { message: '회원이 존재하지 않음' }
-    validates :name, presence: { message: '이름이 존재하지 않음' }
-
-    before_save :set_name_below_max_size, :set_desc_below_max_size
-
-    private
-
-    def set_name_below_max_size
-      return if name.size <= MAX_NAME_SIZE
-
-      self.name = name.truncate(MAX_NAME_SIZE)
-    end
-
-    def set_desc_below_max_size
-      return if description.size <= MAX_DESCRIPTION_SIZE
-
-      self.description = description.truncate(MAX_DESCRIPTION_SIZE)
-    end
-  end
+  validates :user, presence: { message: '값이 존재하지 않음' }
+  validates :name,
+            presence: { message: '값이 존재하지 않음' },
+            length: {
+              maximum: MAX_NAME_SIZE,
+              message: "값이 #{MAX_NAME_SIZE}자를 초과함"
+            }
+  validates :description,
+            length: {
+              maximum: MAX_DESCRIPTION_SIZE,
+              message: "값이 #{MAX_DESCRIPTION_SIZE}자를 초과함"
+            }
+  validates :portfolio, presence: { message: '포트폴리오가 존재하지 않음' }
 end
