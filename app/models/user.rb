bb01fbaea1531caa3fcea5d4df0b6a611ef76522
Bearbeer <kyu2926@gmail.com>
2020-04-22 23:35:09 +0900

    회원 모델 내 비밀번호 validation 버그 픽스

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# 회원 모델
 class User < ActiveRecord::Base
   has_secure_password
   # deleted_at 기반 소프트 딜리트 기능 이용
@@ -9,7 +12,7 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false, message: '값이 이미 존재합니다' },
             length: { in: 6..12, message: '는 6자리 이상 12자리 이하이어야 합니다' },
             format: {
-              with: /\A[a-z]+[a-z0-9]\z/,
+              with: /\A[a-z]+[a-z0-9]{0,13}\z/,
               message: '은 영문 소문자로 시작하는 영문+숫자 조합이어야 합니다'
             }
   validates :password, length: { minimum: 8, message: '는 8자리 이상이어야 합니다' }
