bcec8350ffc0f9c341ba7f4967510ab4cb8f1317
Bearbeer <kyu2926@gmail.com>
2020-04-22 22:50:13 +0900

    User 모델 내 아이디 규칙  Validation 코드 리팩토링

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ActiveRecord::Base
 
   self.table_name = :users
 
-  validates :username, presence: { message: 'ID가 존재하지 않음' },
-            uniqueness: { case_sensitive: false, message: 'ID가 중복됨' },
+  validates :username,
+            uniqueness: { case_sensitive: false, message: '값이 이미 존재합니다' },
+            length: { in: 6..12, message: '는 6자리 이상 12자리 이하이어야 합니다' },
             format: {
-                :with => /\A[a-z]+[a-z0-9]{5,11}\z/,
-                :message => 'ID 정책 위반'
+              with: /\A[a-z]+[a-z0-9]\z/,
+              message: '은 영문 소문자로 시작하는 영문+숫자 조합이어야 합니다'
             }
-  validates :password, presence: { message: '비밀번호가 존재하지 않음' },
-            length: { minimum: 8, message: '비밀번호는 8자리 이상이여야 함' }
+  validates :password, length: { minimum: 8, message: error.invalid(:password) }
   validate :password_with_blank
 
   private
@@ -20,6 +20,6 @@ class User < ActiveRecord::Base
   def password_with_blank
     return if password.present? && !password.include?(' ')
 
-    errors.add :password, '비밀번호에 공백문자 포함'
+    errors.add :password, '에 공백문자가 포함되지 않아야 합니다'
   end
 end
