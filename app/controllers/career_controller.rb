059c4e4c8b4dceb39a16d6fa0dcef19400c093a0
Eun <Eun>
2020-04-23 15:14:47 +0900

    validate params 함수 추가

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -17,16 +17,16 @@ class CareerController < ApiController
 
   # POST /careers
   def create
-    params.require(:name)
-    Career.create(user_id: current_user.id, name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
+    validate_career_params
+    Career.create!(user_id: current_user.id, name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
   end
 
   # PUT /careers/:id
   def update
     validate_params_id
-    params.require(:name)
+    validate_career_params
     career = get_career_by_id params[:id]
-    career.update(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
+    career.update!(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
 
   end
 
@@ -40,6 +40,11 @@ class CareerController < ApiController
 
   def validate_params_id
     params.require(:id)
+  end
+
+  def validate_career_params
+    params.require(:name)
+    params.permit(:name, :description, :start_date, :end_date)
   end
 
   def get_career_by_id(career_id)
