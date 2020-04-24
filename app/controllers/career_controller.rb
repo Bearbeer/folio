0f798d67a465d4afb14c4f0a4a5eeb62ba60eb37
Eun <Eun>
2020-04-24 23:36:09 +0900

    #15 커리어 뷰모델 추가, json 응답 추가

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -4,43 +4,47 @@ class CareerController < ApiController
 
   # GET /careers
   def index
-    careers = Career.find_by(user_id: current_user.id)
-    json(data: { careers: careers })
+    careers = Career.where(user: current_user).order(updated_at: :desc)
+    json(data: { careers: careers_view(careers) })
   end
 
   # Get /careers/:id
   def show
-    validate_params_id
+    params.require(:id)
     career = get_career_by_id params[:id]
-    json(data: { career: career })
+    json(data: { career: career_view(career) })
   end
 
   # POST /careers
   def create
     validate_career_params
     Career.create!(user_id: current_user.id, name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
+
+    json(code: 200, data: { career: career_view(career) })
   end
 
   # PUT /careers/:id
   def update
-    validate_params_id
+    params.require(:id)
     validate_career_params
     career = get_career_by_id params[:id]
     career.update!(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
+
+    json(code: 200, data: { career: career_view(career) })
 
   end
 
   # DELETE /careers/:id
   def destroy
-    validate_params_id
+    params.require(:id)
     career = get_career_by_id params[:id]
     career.destroy
 
+    json(code: 200)
+
   end
 
-  def validate_params_id
-    params.require(:id)
-  end
+  private
 
   def validate_career_params
     params.require(:name)
@@ -54,4 +58,19 @@ class CareerController < ApiController
     career
   end
 
+  def career_view(career)
+    {
+        id: career.id,
+        name: career.name,
+        description: career.description,
+        start_date: career.start_date,
+        end_date: career.end_date,
+        created_at: career.created_at,
+        updated_at: career.updated_at
+    }
+  end
+
+  def careers_view(careers)
+    careers.map { |career| career_view(career) }
+  end
 end
