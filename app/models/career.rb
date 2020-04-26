92755d72725ff0bc7b8fd68307630fb63f4f214d
Bearbeer <kyu2926@gmail.com>
2020-04-26 17:10:12 +0900

    Career, Education, Skill 모델 코드 스타일 최신화 및 리팩토링

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,14 +1,21 @@
 class Career < ActiveRecord::Base 
   acts_as_paranoid
+
   self.table_name = :careers
+
   belongs_to :user, class_name: 'User'
-  validates :name, presence: {message: '경력명이 존재하지 않음'}
-  validate :end_date_after_start_date
+
+  validates :name, presence: { message: '을 입력하세요' }
+  validates :start_date, presence: { message: '을 지정하세요' }
+  validate :validate_end_date
 
   private
 
-  def end_date_after_start_date
-    return if start_date.blank? ||  end_date.blank?
-    raise '날짜 설정 오류' if end_date < start_date
+  # end_date 값이 start_date 값보다 이후인지 확인한다
+  def validate_end_date
+    return if end_date.blank?
+    return if start_date <= end_date
+
+    errors.add :end_date, '종료일시는 시작일시 이후이어야 합니다'
   end
 end
