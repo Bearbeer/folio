1fdd4ddfd08e19c0337bb1bf78ee8dd3b1c11398
Bearbeer <kyu2926@gmail.com>
2020-04-26 17:57:28 +0900

    포트폴리오 하위 프로젝트, 커리어 등록, 수정, 삭제, 목록조회 API 추가

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,8 @@ Rails.application.routes.draw do
   # 중앙 학력 조회/추가/수정/삭제
   resources :educations, controller: 'education', only: %i[index create update destroy]
 
+  resources :portfolios, controller: 'portfolio', only: %i[] do
+    resources :projects, controller: 'portfolio/project', only: %i[index create update destroy]
+    resources :careers, controller: 'portfolio/career', only: %i[index create update destroy]
+  end
 end
