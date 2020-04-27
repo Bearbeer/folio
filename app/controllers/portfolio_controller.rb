f067b2e4606bc17437e2300fd6ce82772397a625
ksee1230 <ksee1230@gmail.com>
2020-04-27 17:04:06 +0900

    portfolio 컨트롤러 1차 리팩토링

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -14,91 +14,56 @@ class PortfolioController < ApiController
   # POST /portfolios
   def create
     params.require(:title)
-    raise Exceptions::BadRequest, 'name 파라미터가 누락되었습니다' unless params.key?(:name)
-    raise Exceptions::BadRequest, 'mobile 파라미터가 누락되었습니다' unless params.key?(:mobile)
-    raise Exceptions::BadRequest, 'email 파라미터가 누락되었습니다' unless params.key?(:email)
-    raise Exceptions::BadRequest, 'gender 파라미터가 누락되었습니다' unless params.key?(:gender)
-    raise Exceptions::BadRequest, 'birthday 파라미터가 누락되었습니다' unless params.key?(:birthday)
-    raise Exceptions::BadRequest, 'address 파라미터가 누락되었습니다' unless params.key?(:address)
-    raise Exceptions::BadRequest, 'memo 파라미터가 누락되었습니다' unless params.key?(:memo)
-    params.require(:portfolio_skills).require(:name)
-    params.require(:portfolio_skills).require(:level)
-    params.require(:portfolio_educations).require(:name)
-    params.require(:portfolio_educations).require(:status)
-    params.require(:portfolio_educations).require(:start_date)
-    params.require(:portfolio_educations).require(:end_date)
-    params.require(:portfolio_projects).require(:name)
-    params.require(:portfolio_projects).require(:description)
-    params.require(:portfolio_careers).require(:name)
-    params.require(:portfolio_careers).require(:description)
-    params.require(:portfolio_careers).require(:start_date)
-    params.require(:portfolio_careers).require(:end_date)
-
-    portfolio = Portfolio::Entity.create!(user: current_user, title: params[:title], name: params[:name],
-                                  mobile: params[:mobile], email: params[:email], gender: params[:gender], 
-                                  birthday: params[:birthday], address: params[:address], memo: params[:memo] )
     
-    params[:portfolio_skills].each do |portfolio_skill|
-      Portfolio::Skill.create!(user: current_user, portfolio: portfolio, name: portfolio_skill[:name], level: portfolio_skill[:level].to_i)
-    end
-
-    params[:portfolio_educations].each do |portfolio_education|
-      Portfolio::Education.create!(user: current_user, portfolio: portfolio, name: portfolio_education[:name],
-                                   status: portfolio_education[:status], start_date: portfolio_education[:start_date],
-                                   end_date: portfolio_education[:end_date])
-    end
-
-    params[:portfolio_projects].each do |portfolio_project|
-      Portfolio::Project.create!(user: current_user, portfolio: portfolio, name: portfolio_project[:name],
-                                 description: portfolio_project[:description])
-    end
-
-    params[:portfolio_careers].each do |portfolio_career|
-      Portfolio::Career.create!(user: current_user, portfolio: portfolio, name: portfolio_career[:name],
-                                description: portfolio_career[:description], start_date: portfolio_career[:start_date],
-                                end_date: portfolio_career[:end_date])
-    end
+    portfolio = Portfolio::Entity.create! attributes.merge(user: current_user)
 
     json(data: { portfolio: portfolio_view(portfolio)})
   end
 
   # PUT /portfolios/:id
   def update
-    params.require(:id)
+    validate_update_params
+    find_and_validate_portfolio
+    update_portfolio
 
-    if params.key?(:title) && params[:title].blank?
-        raise Exceptions::BadRequest, '포트폴리오 제목을 비울 수 없습니다'
-    end
-
-    portfolio = Portfolio::Entity.find_by(id: params[:id], user: current_user)
-    raise Exceptions::NotFound, '포트폴리오가 존재하지 않습니다' unless portfolio
-
-    portfolio.title = params[:title] if params.key?(:title)
-    portfolio.name = params[:name] if params.key?(:name)
-    portfolio.mobile = params[:mobile] if params.key?(:mobile)
-    portfolio.email = params[:email] if params.key?(:email)
-    portfolio.gender = params[:gender] if params.key?(:gender)
-    portfolio.birthday = params[:birthday] if params.key?(:birthday)
-    portfolio.address = params[:address] if params.key?(:address)
-    portfolio.memo = params[:memo] if params.key?(:memo)
-    portfolio.save!
-
-    json(data: { portfolio: only_portfolio_view(portfolio)})
+    json(data: { portfolio: only_portfolio_view(@portfolio)})
   end
 
   # DELETE /portfolios/:id
   def destroy
     params.require(:id)
+    find_and_validate_portfolio
 
-    portfolio = Portfolio::Entity.find_by(id: params[:id], user: current_user)
-    raise Exceptions::NotFound, '포트폴리오가 존재하지 않습니다' unless portfolio
-
-    portfolio.destroy
+    @portfolio.destroy
 
     json(code: 200)
   end
 
   private
+
+  def validate_update_params
+    params.require(:id)
+
+    return unless params.key?(:title) && params[:title].blank?
+    
+    raise Exceptions::BadRequest, '포트폴리오 제목을 비울 수 없습니다'
+  end
+
+  def find_and_validate_portfolio
+    @portfolio = Portfolio::Entity.find_by(id: params[:id], user: current_user)
+    
+    raise Exceptions::NotFound, '포트폴리오가 존재하지 않습니다' unless @portfolio
+  end
+
+  def update_portfolio
+    return if attributes.blank?
+
+    @portfolio.update! attributes
+  end
+
+  def attributes
+    @attributes ||= params.permit(:title, :name, :mobile, :email, :gender, :birthday, :address, :memo).to_h.compact
+  end
 
   def portfolios_view(portfolios)
     portfolios.map { |portfolio| portfolio_view(portfolio) }
