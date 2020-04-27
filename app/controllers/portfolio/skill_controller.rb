c8a4dd3f55357617ea2cf1dab8be79f8194e8af5
Nagion <skrgus2004@naver.com>
2020-04-27 14:38:05 +0900

    포폴 스킬 컨트롤러 피드백 반영

--- app/controllers/portfolio/skill_controller.rb
+++ app/controllers/portfolio/skill_controller.rb
@@ -6,25 +6,20 @@ module Portfolio
     # GET portfolios/:portfolio_id/skills
     def index
       validate_params([:portfolio_id])
+      find_and_validate_portfolio
 
-      skills = Portfolio::Skill.where(
-        user: current_user, 
-        portfolio_id: params[:portfolio_id]
-      ).order(updated_at: :desc)
-
-      json(data: { skills: skills_view(skills) })
+      json(data: { skills: skills_view(@portfolio.skills) })
     end
 
     # POST portfolios/:portfolio_id/skills
     def create
       validate_params([:portfolio_id, :name, :level])
-  
-      skill = Portfolio::Skill.create!(
-        user: current_user, 
-        portfolio_id: params[:portfolio_id], 
-        name: params[:name], 
-        level: params[:level]
-      )
+      find_and_validate_portfolio
+
+      skill = @portfolio.skills.create!(user: current_user, 
+                                        portfolio_id: params[:portfolio_id], 
+                                        name: params[:name], 
+                                        level: params[:level])
   
       json(data: { skill: skill_view(skill) })
     end
@@ -32,33 +27,20 @@ module Portfolio
     # PUT portfolios/:portfolio_id/skills/:id
     def update
       validate_params([:portfolio_id, :id])
+      find_and_validate_portfolio
+      find_and_validate_skill
+      update_skill
   
-      skill = Portfolio::Skill.find_by(
-        id: params[:id], 
-        portfolio_id: params[:portfolio_id], 
-        user: current_user
-      )
-      raise Exceptions::NotFound unless skill
-  
-      skill.name = params[:name] if params.key?(:name)
-      skill.level = params[:level] if params.key?(:level)
-      skill.save!
-  
-      json(data: { skill: skill_view(skill) })
+      json(data: { skill: skill_view(@skill) })
     end
 
     # DELETE portfolios/:portfolio_id/skills/:id
     def destroy
       validate_params([:portfolio_id, :id])
+      find_and_validate_portfolio
+      find_and_validate_skill
   
-      skill = Portfolio::Skill.find_by(
-        id: params[:id], 
-        portfolio_id: params[:portfolio_id],
-        user: current_user
-      )
-      raise Exceptions::NotFound unless skill
-  
-      skill.destroy
+      @skill.destroy
   
       json(code: 200, message: '삭제되었습니다.')
     end
@@ -67,6 +49,25 @@ module Portfolio
 
     def validate_params(props)
       props.each { |prop| params.require(prop) }
+    end
+
+    def find_and_validate_portfolio
+      @portfolio = Portfolio::Entity.find_by(id: params[:portfolio_id],
+                                             user: current_user)
+      
+      raise Exceptions::NotFound, '포트폴리오가 존재하지 않습니다' unless @portfolio
+    end
+    
+    def find_and_validate_skill
+      @skill = @portfolio.skills.find_by(id: params[:id])
+
+      raise Exceptions::NotFound, '기술 스택이 존재하지 않습니다' unless @skill
+    end
+    
+    def update_skill
+      @skill.name = params[:name] if params.key?(:name)
+      @skill.level = params[:level] if params.key?(:level)
+      @skill.save!
     end
 
     ## View Models ##
