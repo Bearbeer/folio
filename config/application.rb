dd30e00a78bdbfe855f6e519d4b0199f12cb7c5e
Nagion <skrgus2004@naver.com>
2020-05-20 16:46:34 +0900

    PUT, DELETE CORS 허용

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Folio
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 
